3b1207385e9c623ebbb06fd50371d7e75547d0db
FreshyMichael <50490309+FreshyMichael@users.noreply.github.com>
2024-01-04 09:20:49 -0500

    Update hammer-the-permalinks.php
    escape the request URL

--- hammer-the-permalinks.php
+++ hammer-the-permalinks.php
@@ -9,39 +9,32 @@
 * License: GNU v3.0
 */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	die();
+if (!defined('ABSPATH')) {
+    die();
 }
 
 /* PermaHammer Start */
 //______________________________________________________________________________
 
-add_action('template_redirect', 'flush_rewrite_rules_on_404');
+add_action('template_redirect', 'permaHammer_flush_rewrite_rules_on_404');
 
-function flush_rewrite_rules_on_404() {
-    if( is_404() ) {
-		//Get The URL of the 404 page
-		$requested_url = (isset($_SERVER['HTTPS']) ? "https" : "http") . "://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
-		
-		//Get the Home URL
-		$siteHomeUrl= get_home_url();
-	    
-	    	//Get the site Admin Email
-	   	$adminEmail = get_bloginfo('admin_email');
-		
-		//Resave Permalinks / Flush Rewrite Rules
-        	flush_rewrite_rules();
-		
-		//Error log that the workaround has been triggered, and include the 404'd URL 
-		error_log('Internal Page 404 Error encountered, flush_rewrite_rules action triggered for url:'. $requested_url );
-	    
-	    	wp_mail( $adminEmail , $siteHomeUrl . ' has encounterned an Internal 404 Error' , $requested_url . ' on ' . $siteHomeUrl . ' triggered the workaround in place. Please visit internal pages and confirm working');
-		
+function permaHammer_flush_rewrite_rules_on_404() {
+    if (is_404()) {
+        // Get The URL of the 404 page
+        $requested_url = esc_url((isset($_SERVER['HTTPS']) ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}");
 
+        // Get the Home URL
+        $siteHomeUrl = get_home_url();
+
+        // Get the site Admin Email
+        $adminEmail = get_bloginfo('admin_email');
+
+        // Resave Permalinks / Flush Rewrite Rules
+        flush_rewrite_rules();
+
+        // Error log that the workaround has been triggered, and include the 404'd URL
+        error_log('Internal Page 404 Error encountered, flush_rewrite_rules action triggered for url:' . $requested_url);
+
+        wp_mail($adminEmail, $siteHomeUrl . ' has encountered an Internal 404 Error', $requested_url . ' on ' . $siteHomeUrl . ' triggered the workaround in place. Please visit internal pages and confirm working');
     }
 }
-
-
-//______________________________________________________________________________
-/* PermaHammer End */
-?>
